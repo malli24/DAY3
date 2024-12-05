86c245b5df4e1cd2eb34094b34ea0a8c97dc0361
malli24 <malleswari82006@gmail.com>
2024-12-05 15:53:53 +0530

    Add files via upload

---
+++ Iffblock.java
@@ -0,0 +1,12 @@
+public class Iffblock{
+    public static void main(String args[])
+    {
+        int a=24;
+        int b=20;
+        if((a^b)==0)
+        {
+        System.out.println("they are equal");
+        }
+        
+    }
+}
