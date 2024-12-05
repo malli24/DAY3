34e7d5580bde9fceb445ed08f8ef1f6cdc2689a8
malli24 <malleswari82006@gmail.com>
2024-12-05 16:14:44 +0530

    Add files via upload

---
+++ small.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class small {
+            public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        {
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+    if(a<b)
+    {
+    if(a<c){
+         System.out.println("a is samller");
+    }else{
+        System.out.println("c is smaller");
+else if(b<c)
+{
+   System.out.println("b is smaller");
+}
+else{
+System.out.println("c is smaller");
+}
+    }
+    
+}
+        }
+        
+    }
+}
+}
