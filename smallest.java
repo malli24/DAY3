754e58068727e55148d601328c6f4d18f453f47b
malli24 <malleswari82006@gmail.com>
2024-12-05 16:17:12 +0530

    Add files via upload

---
+++ smallest.java
@@ -0,0 +1,22 @@
+public class smallest {
+    public static void main(String args[])
+    {
+        int a=40;
+        int b=50;
+        int c=30;
+        System.out.println("the smallest among three is");
+        if(a<b && a<c)
+        {
+            System.out.println("a is smallest");
+        }
+    else if(b<c && b<a)
+{
+    System.out.println("b s smallest");
+}
+else if(c<a && c<b)
+{
+    System.out.println("c is sallest");
+}
+}
+    
+}
